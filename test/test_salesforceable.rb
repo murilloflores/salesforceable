5d29c0c806fed470cb682559bb5995e696f5fb5f
Murillo <murillo.lf@gmail.com>
2015-03-22 21:57:58 -0300

    Adding more tests

--- test/test_salesforceable.rb
+++ test/test_salesforceable.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'salesforceable'
 
-class HolaTest < Minitest::Test
+class SalesforceableTest < Minitest::Test
   
   class MyClass
     
@@ -12,6 +12,17 @@ class HolaTest < Minitest::Test
 
     def save!
       self.save_called = true
+    end
+
+    # This mimetize an ActiveRecord specific behavior, that let's you access properties as in a hash
+    def [](property)
+      if property == 'name'
+        return self.name
+      end
+
+      if property == 'email'
+        return self.email
+      end      
     end
 
     salesforceable_as 'Lead', client_id: 'aa', client_secret: 'bb', fields_mapping: { 'name' => 'salesforce_name', 'email' => 'salesforce_email'}
@@ -57,6 +68,35 @@ class HolaTest < Minitest::Test
       assert_equal nil, obj.salesforce_id
       assert restforce_client_mock.verify
       assert_equal true, obj.save_called
+    end
+
+  end
+
+
+  def test_save_on_salesforce
+
+    obj = MyClass.new()
+    obj.salesforce_id = nil
+    obj.save_called = false
+    obj.name = 'Zé da Silva'
+    obj.email = 'ze@padaria.com'
+    
+    restforce_client_mock = Minitest::Mock.new
+    restforce_client_mock.expect(:create!, 'SALESFORCEID8888', ['Lead', { 'salesforce_name' => 'Zé da Silva', 'salesforce_email' => 'ze@padaria.com'}])
+
+    mock_wrapper = lambda { |connection_params|
+      assert_equal 'aa', connection_params[:client_id]
+      assert_equal 'bb', connection_params[:client_secret]
+      assert_equal '1111111', connection_params[:refresh_token]
+      assert_equal '2222222', connection_params[:instance_url]
+      restforce_client_mock
+    }
+
+    Restforce.stub :new, mock_wrapper do
+      obj.save_on_salesforce!('1111111','2222222')
+      assert restforce_client_mock.verify
+      assert_equal true, obj.save_called
+      assert_equal 'SALESFORCEID8888', obj.salesforce_id
     end
 
   end
